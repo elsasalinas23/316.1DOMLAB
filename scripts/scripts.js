c0ad232d943f9bbbdc32d4f4c236b3307a074bcb
Elsa S <elsasalinas2388@gmail.com>
2025-08-11 16:01:01 -0400

    fixed part three

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,37 @@
 //cPart one 
-const mainEl = document.querySelector('main');
+let mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr');
 
 // Part Two 
 
-const topMenuEl = document.getElementById('top-menu');
+let topMenuEl = document.getElementById('top-menu');
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var( --top-menu-bg)';
 topMenuEl.classList.add('flex-around');
 
+
+
 // Part Three 
 // Menu data structure
 var menuLinks = [
-  { text: 'about', href: '/about' },
-  { text: 'catalog', href: '/catalog' },
-  { text: 'orders', href: '/orders' },
-  { text: 'account', href: '/account' },
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
 ];
-// loop and set const for a 
-menuLinks.forEach(("link")) => {
-    const a = document.createElement("a");
-    //set where it points to
-    a.setAttribute("href", menuLinks.href);
-    //set the words that hsow up on the button
-    a.textContent = menuLinks.text.toUpperCase(); 
-    // put <a> inside top menu bar
-    topMenuEl.appendChild(a);
+//Iterate over the entire menuLinks array and for each "link" object:
+
+for (let link of menuLinks) {
+    //  // Create an <a> element.
+    let menuButton = document.createElement('a');
+     //  // On the new element, add an href attribute with its value set to the href property of the "link" object.
+     menuButton.setAttribute("href", link.href);
+    //  // Set the new element's content to the value of the text property of the "link" object.
+     menuButton.textContent = link.text;
+    // console.log(menuButton);
+    //  // Append the new element to the topMenuEl element.
+    topMenuEl.appendChild(menuButton);
 
 }
