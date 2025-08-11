d5bd1edd8b6df4fc1a4e2382d1ba77ec9943a9e6
Elsa S <elsasalinas2388@gmail.com>
2025-08-11 15:02:01 -0400

    part two/three

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,32 @@
+//cPart one 
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr');
+
+// Part Two 
+
+const topMenuEl = document.getElementById('top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var( --top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+// Part Three 
+// Menu data structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+// loop and set const for a 
+menuLinks.forEach(("link")) => {
+    const a = document.createElement("a");
+    //set where it points to
+    a.setAttribute("href", menuLinks.href);
+    //set the words that hsow up on the button
+    a.textContent = menuLinks.text.toUpperCase(); 
+    // put <a> inside top menu bar
+    topMenuEl.appendChild(a);
+
+}
